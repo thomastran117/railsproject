c7269c463be2e973a7b4b08d91b0251181a0f364
thomastran117 <thomas.tran.1591@gmail.com>
2026-01-28 22:22:00 -0500

    api prefix

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", :as => :rails_health_check
-  scope :auth do
-    post :signup, to: "auth#signup"
-    post :login, to: "auth#login"
-    get :me, to: "auth#me"
+  scope :api do
+    scope :auth do
+      post :signup, to: "auth#signup"
+      post :login, to: "auth#login"
+      get :me, to: "auth#me"
+    end
   end
 end
